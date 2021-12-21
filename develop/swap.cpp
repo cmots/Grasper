140216a448baa060f4b9924823e19267ebdfce4d
cmots <1061129075@qq.com>
2021-12-21 17:43:32 +0800

    add swap policy function but didn't complete I/O swap out&in

---
+++ develop/swap.cpp
@@ -0,0 +1,162 @@
+#include "swap.hpp"
+#include "storage/vkvstore.hpp"
+#include "storage/ekvstore.hpp"
+
+#include <cstdlib.h>
+/**
+ * @brief  get the difference between current time and last check time
+ * @note   if now < time, it turns to next round, like carrying
+ * @param  time: last check time
+ * @retval time diff between last time and current
+ */
+uint16_t swap::getIdleTime(uint16_t time)
+{
+    uint16_t now = getCurrentTime();
+    if (now >= time)
+        return now - time;
+    return 65535 - time + now;
+}
+
+/**
+ * @brief  increase the counter of a gobj
+ * @note   Use lfu_incr_factor to control the increase ratio, larger factor means smaller ratio
+ *         Also, larger counter causes smaller increase ratio
+ * @param  &gobj: 
+ * @retval 
+ */
+uint8_t swap::LFUIncreaseAndReturn(gobj &gobj)
+{
+    uint8_t counter = gobj.counter;
+    if (counter == 255)
+        return 255;
+    double r = (double)rand() / RAND_MAX;
+    double baseval = counter - init_counter;
+    if (baseval < 0)
+        baseval = 0;
+    double p = 1.0 / (baseval * lfu_incr_factor + 1);
+    if (r < p)
+    {
+        counter++;
+        gobj.counter = counter;
+    }
+    return counter;
+}
+
+/**
+ * @brief  decrease the counter of a gobj
+ * @note   
+ * @param  &gobj: the gobj to be operated
+ * @retval counter after decrease
+ */
+uint8_t swap::LFUDecreaseAndReturn(gobj &gobj)
+{
+    uint16_t last_time = gobj.time;
+    uint8_t counter = gobj.counter;
+    uint16_t num_periods = getIdleTime(last_time) / ((_interval << 16) & 65535);
+    if (num_periods)
+    {
+        counter = num_periods > counter ? 0 : counter - num_periods;
+        gobj.counter = counter;
+    }
+    return counter;
+}
+
+/**
+ * @brief  Create a new grasper object
+ * @note   set its counter to init_counter, or it may be swapped after init
+ * @param  key: the key of grasper object
+ * @retval the newly create grasper object
+ */
+gobj *swap::createGrasperObj(ikey_t key)
+{
+    gobj *gobj = malloc(sizeof(gobj));
+    gobj->key = key;
+    gobj->counter = init_counter;
+    gobj->time = getCurrentTime();
+    keys.push_back(gobj);
+    return gobj;
+}
+
+/**
+ * @brief  get 16 high bits of current unix time => minute
+ * @note   
+ * @retval the current time in unit of minute
+ */
+uint16_t swap::getCurrentTime()
+{
+    return (std::time(0) / 60) & 65535;
+}
+
+bool swap::swapOut(int pos)
+{
+    return true;
+
+    obj = _gobjs.at(pos);
+    ikey_t key = obj.key;
+    value_t &val;
+
+    VKVStore::get_property_local(key.pid, val);
+    string fname = key.pid;
+    ofstream out_f(fname, ios::binary);
+
+    if (!out_f.is_open())
+    {
+        return false;
+    }
+
+    out_f.write((char *)val.content);
+
+    out_f.close();
+
+    return true;
+}
+
+bool swap::swapIn(int pos)
+{
+    return true;
+    ifstream in_f(fn, ios::binary);
+
+    if (!in_f.is_open())
+    {
+        return false;
+    }
+
+    char *mem = get<2>(data);
+
+    uint64_t last_entry, mem_sz;
+
+    in_f.read((char *)&last_entry, sizeof(uint64_t));
+    in_f.read((char *)&mem_sz, sizeof(uint64_t));
+    in_f.read(mem, mem_sz);
+    in_f.close();
+
+    data = make_tuple(last_entry, mem_sz, mem);
+
+    return true;
+}
+
+void swap::LFUSwap(unsigned int interval, int pool_size)
+{
+    while (true)
+    {
+        srand((unsigned)time(NULL));
+        vector<pair(gobj, int)> tmp_gobjs;
+        for (int i : pool_size)
+        {
+            int r = rand() % _gobjs.size();
+            LFUDecreaseAndReturn(&_gobjs.at(r));
+            tmp_gobjs.push_back(pair(_gobjs.at(r), r));
+        }
+        std::sort(tmp_gobjs.begin(), tmp_gobjs.end(), comp);
+        swapOut(tmp_gobjs.at[0].second);
+        std::this_thread::sleep_for(std::chrono::milliseconds(interval * 100));
+    }
+}
+
+inline bool comp(const pair(gobj, int) & a, const pair(gobj, int) & b)
+{
+    if (a.first.counter <= b.first.counter)
+        return true;
+    else
+        return false;
+}
---
+++ develop/swap.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "type.hpp"
+#include <ctime>
+#include <thread>
+#include <vector>
+#include <thread>
+#include <string>
+#include <stdio.h>
+
+typedef struct grasperObject {    // Grasper Object
+    uint16_t time; // last check time (relative to server.lruclock) :16bit
+    int counter;   // reference counter:8 bits
+    ikey_t key;
+} gobj;
+
+class swap
+{
+private:
+    unsigned int _interval;
+    unsigned int _pool_size;
+    int lfu_incr_factor;
+    uint8_t init_counter;
+    std::time_t currentTime;    //useless
+    bool swapOut(vector<gobj> objs);
+    bool swapIn(vector<gobj> objs);
+    vector<gobj> _gobjs;
+
+
+public:
+    swap(int interval, int pool_size);
+    ~swap();
+    uint64_t getIdleTime(uint16_t time);
+    uint8_t LFUIncreaseAndReturn(gobj &gobj);
+    uint8_t LFUDecreaseAndReturn(gobj &gobj);
+    gobj* createGrasperObj(ikey_t key);
+    uint16_t getCurrentTime();
+    void LFUSwap(int interval, int pool_size);
+    
+};
+
+swap::swap(unsigned int interval,unsigned int pool_size, int factor, uint8_t counter)
+{
+    this->_interval = internal;
+    this->_pool_size = pool_size;
+    this->lfu_incr_factor = factor;
+    this->init_counter = counter;
+    this->currentTime = std::time(0);
+    std::thread swap_t(LFUSwap, interval, pool_size);
+}
+
+swap::~swap()
+{
+    vector<gobj>.swap(_gobjs);
+}
